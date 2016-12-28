a6effbdeb05318e406dc464d3f65c1f9f35db147
Thanggun99 <thanggun99@gmail.com>
2016-12-28 19:11:13 +0700

    Update API.java

--- API.java
+++ API.java
@@ -16,34 +16,54 @@ import java.util.Map;
  */
 
 public class API {
-    public final static int GET = 1;
-    public final static int POST = 2;
     public static final String SCHEME = "http";
     public static final String HOST = "192.168.56.1";
     public static final String PATH = "WebService";
-    public static final String BAN_URL = "GetBan.php";
-    public static final String NHOM_MON_URL = "GetNhomMon.php";
-    public static final String THUC_DON_URL = "GetThucDon.php";
-    public static final String HOA_DON_URL = "GetHoaDon.php";
-    public static final String TAO_MOI_HOA_DON_URL = "TaoMoiHoaDon.php";
-    public static final String UPDATE_BAN_URL = "UpdateBan.php";
-    public static final String UPDATE_THUC_DON_ORDER_URL = "UpdateThucDonOrder.php";
-    public static final String THEM_THUC_DON_ORDER_URL = "ThemThucDonOrder.php";
-    public static final String DELETE_MON_ORDER_URL = "DeleteMonOrder.php";
+    public static final String PATH_BAN = "BanService/";
+    public static final String PATH_THUC_DON = "ThucDonService/";
+    public static final String PATH_HOA_DON = "HoaDonService/";
+    public static final String PATH_DAT_TRUOC = "DatTruocService/";
+    public static final String PATH_NHOM_MON = "NhomMonService/";
 
-    public static String callService(String path, int method, Map<String, String> paramsGet, Map<String, String> paramsPost) {
+    public static final String GET_BAN_URL = PATH_BAN + "GetBan.php";
+    public static final String UPDATE_BAN_URL = "UpdateBan.php";
+
+    public static final String GET_THUC_DON_URL = PATH_THUC_DON + "GetThucDon.php";
+
+    public static final String GET_HOA_DON_URL = PATH_HOA_DON + "GetHoaDon.php";
+    public static final String TAO_MOI_HOA_DON_URL = PATH_HOA_DON + "TaoMoiHoaDon.php";
+    public static final String TINH_TIEN_HOA_DON_URL = PATH_HOA_DON + "TinhTienHoaDon.php";
+    public static final String DELETE_HOA_DON_URL = PATH_HOA_DON + "DeleteHoaDon.php";
+
+    public static final String GET_DAT_TRUOC_URL = PATH_DAT_TRUOC + "GetDatTruoc.php";
+    public static final String DAT_TRUOC_URL = PATH_DAT_TRUOC + "DatTruoc.php";
+    public static final String UPDATE_DAT_TRUOC_URL = PATH_DAT_TRUOC + "UpdateDatTruoc.php";
+    public static final String DELETE_DAT_TRUOC_URL = PATH_DAT_TRUOC + "DeleteDatTruoc.php";
+
+    public static final String GET_NHOM_MON_URL = PATH_NHOM_MON + "GetNhomMon.php";
+
+    public static final String UPDATE_THUC_DON_ORDER_URL = PATH_HOA_DON + "UpdateThucDonOrder.php";
+    public static final String UPDATE_GIAM_GIA_URL = PATH_HOA_DON + "UpdateGiamGia.php";
+    public static final String THEM_THUC_DON_ORDER_URL = PATH_HOA_DON + "ThemThucDonOrder.php";
+    public static final String DELETE_MON_ORDER_URL = PATH_HOA_DON + "DeleteMonOrder.php";
+
+    public static String callService(String path, Map<String, String> getParams) {
+        return callService(path, getParams, null);
+    }
+
+    public static String callService(String path, Map<String, String> getParams, Map<String, String> postParams) {
         HttpURLConnection connect;
-        InputStream is = null;
+        InputStream is;
         String response = null;
 
         Uri.Builder builder = new Uri.Builder()
                 .scheme(SCHEME)
                 .authority(HOST)
                 .appendPath(PATH)
-                .appendPath(path);
+                .appendEncodedPath(path);
 
-        if (paramsGet != null) {
-            builder = Utils.builderParams(builder, paramsGet);
+        if (getParams != null) {
+            builder = Utils.builderParams(builder, getParams);
         }
         try {
             connect = (HttpURLConnection) new URL(builder.build().toString()).openConnection();
@@ -51,9 +71,9 @@ public class API {
             connect.setRequestProperty("Connection", "close");
             connect.setDoInput(true);
 
-            if (method == POST && paramsPost != null) {
+            if (postParams != null) {
                 Uri.Builder builderPostParams = new Uri.Builder();
-                builderPostParams = Utils.builderParams(builderPostParams, paramsPost);
+                builderPostParams = Utils.builderParams(builderPostParams, postParams);
 
                 connect.setRequestProperty("Content-Type",
                         "application/x-www-form-urlencoded;charset=UTF-8");
